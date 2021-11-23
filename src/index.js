deaa20d735eb1eaddfebb94d2d043d5e549ee2c5
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-11-23 10:58:30 +0200

    adds swiper slider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './sass/main.scss';
 import "@fontsource/raleway";
 import "@fontsource/open-sans";
+import Swiper from '../node_modules/swiper';
+import '../node_modules/swiper/swiper.scss';
+
+  const swiper = new Swiper('.swiper');
+
