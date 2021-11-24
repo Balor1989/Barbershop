d7dd9e77c7d13311851c24cc7f2a7f42ca65fe01
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-11-24 13:52:00 +0200

    adds back-to-top btn

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import './sass/main.scss';
 import "@fontsource/raleway";
 import "@fontsource/open-sans";
 import Swiper, {Autoplay, EffectFade, Navigation, Pagination } from 'swiper';
+import { addBackToTop } from 'vanilla-back-to-top'
 
+// Swiper settings
 Swiper.use([Navigation, Pagination, Autoplay, EffectFade]);
-
 new Swiper('.swiper', {
   direction:'vertical',
   speed: 700,
@@ -28,3 +29,14 @@ new Swiper('.swiper', {
  },
 });
 
+
+// Back-to-top button
+addBackToTop({
+  backgroundColor: 'transparent',
+  diameter: 50,
+  innerHTML:'<svg id="circle-up" viewBox="0 0 32 32"><path d="M0 16c0 8.837 7.163 16 16 16s16-7.163 16-16-7.163-16-16-16-16 7.163-16 16zM29 16c0 7.18-5.82 13-13 13s-13-5.82-13-13 5.82-13 13-13 13 5.82 13 13z"></path><path d="M22.086 20.914l2.829-2.829-8.914-8.914-8.914 8.914 2.828 2.828 6.086-6.086z"></path></svg>',
+  textColor: '$primary-text-color',
+  scrollDuration: 700,
+  showWhenScrollTopIs: 400,
+  cornerOffset: 30,
+})
