b5d2d2427844f2be602f1f51ab9a1b7c52807736
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-11-24 15:34:45 +0200

    adds smooth-scroll and fixs mobile nemu hover and focus links

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import './sass/main.scss';
 import "@fontsource/raleway";
 import "@fontsource/open-sans";
 import Swiper, {Autoplay, EffectFade, Navigation, Pagination } from 'swiper';
-import { addBackToTop } from 'vanilla-back-to-top'
+import { addBackToTop } from 'vanilla-back-to-top';
+import SweetScroll from 'sweet-scroll';
+
+
 
 // Swiper settings
 Swiper.use([Navigation, Pagination, Autoplay, EffectFade]);
@@ -34,9 +37,20 @@ new Swiper('.swiper', {
 addBackToTop({
   backgroundColor: 'transparent',
   diameter: 50,
-  innerHTML:'<svg id="arrow-up2" viewBox="0 0 32 32"><path d="M27.414 12.586l-10-10c-0.781-0.781-2.047-0.781-2.828 0l-10 10c-0.781 0.781-0.781 2.047 0 2.828s2.047 0.781 2.828 0l6.586-6.586v19.172c0 1.105 0.895 2 2 2s2-0.895 2-2v-19.172l6.586 6.586c0.39 0.39 0.902 0.586 1.414 0.586s1.024-0.195 1.414-0.586c0.781-0.781 0.781-2.047 0-2.828z"></path></svg>',
+  innerHTML: '<svg id="arrow-up2" viewBox="0 0 32 32"><path d="M27.414 12.586l-10-10c-0.781-0.781-2.047-0.781-2.828 0l-10 10c-0.781 0.781-0.781 2.047 0 2.828s2.047 0.781 2.828 0l6.586-6.586v19.172c0 1.105 0.895 2 2 2s2-0.895 2-2v-19.172l6.586 6.586c0.39 0.39 0.902 0.586 1.414 0.586s1.024-0.195 1.414-0.586c0.781-0.781 0.781-2.047 0-2.828z"></path></svg>',
   textColor: '$primary-text-color',
   scrollDuration: 700,
   showWhenScrollTopIs: 400,
   cornerOffset: 30,
-})
+});
+
+// Smooth-scroll settings
+const onSmoothScroll = () => {
+  const scroller = new SweetScroll({
+    easing: 'easeOutQuad'
+  });
+};
+document.addEventListener(
+  'DOMContentLoaded',onSmoothScroll,
+  false,
+);
